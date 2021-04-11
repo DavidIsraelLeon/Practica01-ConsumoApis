5d042aa9fceff0f75466b14ffd88247fe7e88ce1
David Leon <dilggallardos@hotmail.com>
2021-04-11 18:53:54 -0500

    Colocando Boostrap

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/50/'
+
+fetch(url)
+.then(response => response.json())
+.then(data=>{
+    let element = document.getElementById("elem")
+    element.innerHTML=
+    '<p>${data.name}</p>'
+    '<p>${data.order}</p>'
+    
+
+    console.log(data)
+
+})
+.catch(err=>console.log(err))
